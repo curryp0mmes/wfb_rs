ebc0969e1a1b795ed0945145892bb44b3d506537
Simon <simonausbs@gmail.com>
2025-05-06 17:23:53 +0200

    changed to raw sockets instead of high level udp (important for later)

--- src/tx.rs
+++ src/tx.rs
@@ -1,4 +1,12 @@
-use std::{fmt::format, net::UdpSocket, vec};
+use std::os::fd::AsRawFd;
+use std::vec;
+use nix::net::if_::if_nametoindex;
+use nix::sys::ioctl;
+use nix::sys::socket::{self, SockaddrIn, UnixAddr};
+use nix::unistd::close;
+use std::net::Ipv4Addr;
+use std::os::unix::io::RawFd;
+
 
 pub struct Transmitter {
     radio_port: u16,
@@ -59,29 +67,95 @@ impl Transmitter {
     }
 
     pub fn run(&self) {
-        //Bind to all wifi devices
-
         println!("Binding {} to Port {}", self.wifi_device, self.udp_port);
-        let udp_receiver = UdpSocket::bind(format!("0.0.0.0:{}", self.udp_port))
-            .expect("Failed to bind to udp port");
+        let udp_file_descriptor = match open_udp_socket_for_rx(
+            socket::SockaddrIn::new(0,0,0,0, self.udp_port),
+            self.buffer_size,
+            0, // Bind to all interfaces
+            socket::SockType::Datagram,
+            socket::SockProtocol::Udp,
+        ) {
+            Ok(fd) => fd,
+            Err(e) => {
+                println!("Error opening UDP socket: {:?}", e);
+                return;
+            }
+        };
+
+        println!("UDP socket opened with fd: {}", udp_file_descriptor);
 
         assert!(self.buffer_size > 0);
         let mut buffer: Vec<u8> = vec![0; self.buffer_size];
         //TODO own thread for the udp socket polling
         loop {
-            match udp_receiver.recv(&mut buffer) {
-                Ok(datasize) => {
-                    println!("Received data with length {}: {:?}", datasize, buffer);
-
-                    // TODO header
-
-                    // TODO inject packet into wifistick 
-
-                }
-                Err(e) => {
-                    println!("Error: {:?}", e);
-                }
-            }
+            
         }
     }
 }
+
+
+fn open_udp_socket_for_rx(
+    socket_address: SockaddrIn,
+    rcv_buf_size: usize,
+    bind_addr: u32,
+    socket_type: socket::SockType,
+    socket_protocol: socket::SockProtocol,
+) -> Result<RawFd, nix::Error> {
+    // Create socket
+    let file_descriptor = socket::socket(
+        socket::AddressFamily::Inet,
+        socket_type,
+        socket::SockFlag::empty(),
+        socket_protocol,
+    )?;
+
+    // Set SO_REUSEADDR
+    socket::setsockopt(&file_descriptor, socket::sockopt::ReuseAddr, &true)?;
+
+    // Set SO_RXQ_OVFL
+    socket::setsockopt(&file_descriptor, socket::sockopt::RxqOvfl, &1)?;
+
+    // Set SO_RCVBUF if specified
+    if rcv_buf_size > 0 {
+        socket::setsockopt(&file_descriptor, socket::sockopt::RcvBuf, &rcv_buf_size)?;
+    }    
+
+    // Bind
+    if let Err(e) = socket::bind(file_descriptor.as_raw_fd(), &socket_address) {
+        let _ = close(file_descriptor);
+        return Err(e);
+    }
+
+    Ok(file_descriptor.as_raw_fd())
+}
+
+/*TODO 
+fn open_socket_for_interface(
+
+) -> Result<RawFd, nix::Error> {
+
+    let interface_name = "wlan0";
+
+    let file_descriptor = socket::socket(
+        socket::AddressFamily::Packet,
+        socket::SockType::Raw,
+        socket::SockFlag::empty(),
+        socket::SockProtocol::Raw,
+    )?;
+
+    let ifindex = if_nametoindex(interface_name).expect("Failure");
+
+    let sockaddr = socket::
+    socket::bind(
+        file_descriptor.as_raw_fd(),
+        sockaddr,
+        
+    )?;
+    
+
+
+
+    Ok(file_descriptor.as_raw_fd())
+}
+
+*/
